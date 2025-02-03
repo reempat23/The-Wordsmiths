f2dacdccf0e3deae626d104f8b9c1a06955dd97c
reempat23 <28376@tn.ac.th>
2025-02-03 21:55:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -77,5 +77,5 @@ const goToAnotherPage = document.getElementById("go-to-another-page");
 
 // เพิ่มเหตุการณ์คลิกเพื่อเปิดหน้าเว็บอื่น
 goToAnotherPage.addEventListener("click", () => {
-    window.location.href = "https://script.google.com/a/macros/tn.ac.th/s/AKfycbzQ8BotB2t0q1a5PYm34qiKPB6v1cJ_JwEPo95-A1Sk/dev"; // เปลี่ยนลิงก์เป็นเว็บที่ต้องการ
+    window.location.href = "https://script.google.com/a/macros/tn.ac.th/s/AKfycbzpDg7x_K8AJKu3F1serqwaDAl1zpLRoCW7dtMyIvBOftDW-I_Ov0_CdgbgLe8HIt4wmw/exec"; // เปลี่ยนลิงก์เป็นเว็บที่ต้องการ
 });
